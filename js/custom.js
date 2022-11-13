76bd552c8ce6c6d97a52557d31b93c8a83fdd9ac
Tuhin ahamed <118074544+foyazahamed@users.noreply.github.com>
2022-11-12 23:41:28 -0800

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+$(function(){
+    new VenoBox({
+        selector: '.my-video-links',
+    });
+})
+
+$(function(){
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+})
